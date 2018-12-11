e746ba1fcc99ebbd33ae2589aa10242790a9b54b
Aylin DeBruyne <adebruyne88@gmail.com>
2018-12-11 10:07:10 -0500

    maps over dogs to draw each one to screen

--- src/Dogs.js
+++ src/Dogs.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Dogs(props) {
+  const dogListItems = props.dogList.map((dogName, index) => {
+    return (
+      <li key={index}>
+        <Link to={`/dogs/${dogName}`}>{dogName}</Link>
+      </li>
+    );
+  });
   return (
     <div>
       <h1>DOGS!</h1>
+      <ul>{dogListItems}</ul>
     </div>
   );
 }
