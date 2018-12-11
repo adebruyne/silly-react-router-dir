bd9d0605f8cf78b31b93edc9fa0eb75275152191
Aylin DeBruyne <adebruyne88@gmail.com>
2018-12-10 20:20:26 -0500

    imports react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import {
+  BrowserRouter as Router, // Watches for changes to URL
+  Route // Renders components based on the specific URL
+  //Link                     // Changes the URL so the Router sees it
+} from "react-router-dom";
+
 class App extends Component {
   constructor(props) {
     super(props);
